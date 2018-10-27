622d6f9fffa250bc34342ca26a7b1f41cb501e2c
nathanshangkala <shangkal@usc.edu>
2018-10-26 21:09:23 -0700

    Add Assets

---
+++ 201FinalProject/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private float walkSpeed;
+    private float curSpeed;
+    private float maxSpeed;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        curSpeed = 1.0f;
+        maxSpeed = curSpeed;
+        // Move senteces
+        rb.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+                                             Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+    }
+}
