242f75930a38f1c2059106c6a896cbb002d53d43
nathanshangkala <shangkal@usc.edu>
2018-11-03 12:33:47 -0700

    Add Game Manager

---
+++ 201FinalProject/Assets/BattleOnCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleOnCollision : MonoBehaviour {
+
+    public GameObject Other;
+    private Renderer renderer;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        renderer = GetComponent<Renderer>();
+        //renderer.enabled = false;
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void OnCollisionStay2D(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Colliding");
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	}
+}
---
+++ 201FinalProject/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Awake ()
+    {
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(player);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
