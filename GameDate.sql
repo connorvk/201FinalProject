15f11802dd1da44e015f266d378b8babebcc518d
BaiyuZhu <baiyuzhu@usc.edu>
2018-11-13 17:35:12 -0800

    Add files via upload
    Newest database

--- GameDate.sql
+++ GameDate.sql
@@ -3,18 +3,24 @@ USE Info;
 CREATE TABLE Userinfo(
 	SaveID INT(11) AUTO_INCREMENT PRIMARY key,
 	Username VARCHAR(255),
-    HashedPassword VARCHAR(255)
+    UserPassword VARCHAR(255)
 );
 
-CREATE TABLE Monster(
+CREATE TABLE Market(
 	MonsterID INT(11) AUTO_INCREMENT PRIMARY key,
+    MonsterType VARCHAR(255),
 	MonsterName VARCHAR(255),
     HP INT(11),
-    Attack INT(11)
+    Attack INT(11),
+    SaveID INT (11),
+    Cost VARCHAR(255)
 );
 
 CREATE TABLE Inventory(
+	SaveID INT(11),
+    Username VARCHAR(255),
 	MonterType VARCHAR(255),
     Monstername VARCHAR(255),
-    CurrentHP INT(11)
+    CurrentHP INT(11),
+    Attack INT(11)
 );
