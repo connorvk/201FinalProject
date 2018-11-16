86812f21da4f0acc1521c9cd71b955d687055c1f
Vincent Liang <liangvin@usc.edu>
2018-11-16 15:43:32 -0800

    update

--- GameDate.sql
+++ GameDate.sql
@@ -1,26 +1,26 @@
+DROP DATABASE IF EXISTS Info;
 CREATE DATABASE Info;
 USE Info;
+
 CREATE TABLE Userinfo(
 	SaveID INT(11) AUTO_INCREMENT PRIMARY key,
 	Username VARCHAR(255),
-    UserPassword VARCHAR(255)
-);
-
-CREATE TABLE Market(
-	MonsterID INT(11) AUTO_INCREMENT PRIMARY key,
-    MonsterType VARCHAR(255),
-	MonsterName VARCHAR(255),
-    HP INT(11),
-    Attack INT(11),
-    SaveID INT (11),
-    Cost VARCHAR(255)
+	UserPassword VARCHAR(255)
 );
 
 CREATE TABLE Inventory(
-	SaveID INT(11),
-    Username VARCHAR(255),
-	MonterType VARCHAR(255),
+	MonsterID INT(11) AUTO_INCREMENT PRIMARY key,
+	Username VARCHAR(255),
     Monstername VARCHAR(255),
-    CurrentHP INT(11),
-    Attack INT(11)
+	HP INT(11),
+	Attack INT(11)
 );
+
+CREATE TABLE Market(
+    Username VARCHAR(255),
+	Monstername VARCHAR(255),
+    Ask VARCHAR(255),
+	HP INT(11),
+	Attack INT(11)
+);
+
