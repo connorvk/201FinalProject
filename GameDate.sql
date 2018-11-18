49c415d0f5c4427474021fb146644fb771e76841
Vincent Liang <liangvin@usc.edu>
2018-11-17 21:23:44 -0800

    update

--- GameDate.sql
+++ GameDate.sql
@@ -3,17 +3,14 @@ CREATE DATABASE Info;
 USE Info;
 
 CREATE TABLE Userinfo(
-	SaveID INT(11) AUTO_INCREMENT PRIMARY key,
+	SaveID INT(11) AUTO_INCREMENT PRIMARY KEY,
 	Username VARCHAR(255),
 	UserPassword VARCHAR(255)
 );
 
-CREATE TABLE Inventory(
-	MonsterID INT(11) AUTO_INCREMENT PRIMARY key,
+CREATE TABLE Inventory(	
 	Username VARCHAR(255),
-    Monstername VARCHAR(255),
-	HP INT(11),
-	Attack INT(11)
+    Userinventory VARCHAR(1023)
 );
 
 CREATE TABLE Market(
